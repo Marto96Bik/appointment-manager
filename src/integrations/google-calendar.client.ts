277c5f305f3d17dc3745c837622f33866b0b8be7
Marto96Bik <martobik@gmail.com>
2026-01-09 16:00:32 +0200

    integration google calendar

---
+++ src/integrations/google-calendar.client.ts
@@ -0,0 +1,35 @@
+import { google } from "googleapis";
+require("dotenv").config();
+
+export class GoogleCalendarClient {
+  private calendar;
+
+  constructor() {
+    // refresh_token auth
+    const oauth2Client = new google.auth.OAuth2(
+      process.env.GOOGLE_CLIENT_ID,
+      process.env.GOOGLE_CLIENT_SECRET,
+      "urn:ietf:wg:oauth:2.0:oob"
+    );
+
+    oauth2Client.setCredentials({
+      refresh_token: process.env.REFRESH_TOKEN,
+    });
+
+    this.calendar = google.calendar({ version: "v3", auth: oauth2Client });
+  }
+
+  async listEvents(fromDate: Date, maxResults?: number) {
+    const res = await this.calendar.events.list({
+      calendarId: "primary",
+      timeMin: fromDate.toISOString(),
+      maxResults: maxResults,
+      singleEvents: true,
+      orderBy: "startTime",
+    });
+
+    return res.data.items ?? [];
+  }
+
+  async createEvent(data: { title: string; date: string }) {}
+}
