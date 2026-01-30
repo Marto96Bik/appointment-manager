246fe0a40d745d98c5a5c923afe6ab5fff0deb04
Marto96Bik <martobik@gmail.com>
2026-01-30 18:10:41 +0200

    refactor: appointment dto patientId string transform to number

--- src/app/api/appointment/appointent.dto.ts
+++ src/app/api/appointment/appointent.dto.ts
@@ -3,7 +3,7 @@ import { z } from "zod";
 export const createAppointmentSchema = z.object({
   start: z.iso.datetime({ local: true }),
   end: z.iso.datetime({ local: true }),
-  patientId: z.number().positive(),
+  patientId: z.coerce.number().positive(),
 });
 
 export const getAppointmentsSchema = z.object({
