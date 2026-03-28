d5c132c6554ab960dabfd5b971ca3b1eee50a9cb
Marto96Bik <martobik@gmail.com>
2026-03-28 23:02:41 +0300

    fix pino logger error on deploy

--- src/lib/utils/logger.ts
+++ src/lib/utils/logger.ts
@@ -1,15 +1,2 @@
 import pino from "pino";
-
-let logger;
-
-if (process.env.NODE_ENV === "production") {
-  logger = pino();
-} else {
-  logger = pino({
-    transport: {
-      target: "pino-pretty",
-    },
-  });
-}
-
-export { logger };
+export const logger = pino();
