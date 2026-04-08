a49af697e19ee0a51a8b86fb4a43f2d8d874cf05
Marto96Bik <martobik@gmail.com>
2026-04-08 12:06:32 +0300

    refactor: fix login createOAuthClient implementation

--- src/app/api/auth/login/route.ts
+++ src/app/api/auth/login/route.ts
@@ -1,8 +1,9 @@
 import { NextResponse } from "next/server";
-import { createOAuthClient } from "../../../../integrations/google-auth.client";
+import { createOAuthClient } from "@/integrations/google-auth.client";
 
 export async function GET() {
-  const url = createOAuthClient.generateAuthUrl({
+  const oauth2Client = createOAuthClient();
+  const url = oauth2Client.generateAuthUrl({
     access_type: "offline", // Vital para el refresh_token
     prompt: "consent", // Fuerza a Google a mostrar la pantalla de permiso y dar el refresh_token
     scope: ["openid", "email", "profile", "https://www.googleapis.com/auth/calendar.events"],
